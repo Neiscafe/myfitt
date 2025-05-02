eb15efaaca92650594950919eefa439f2a6a8fd4
Guilherme Neis <guilherme.neis@deltasul.com.br>
2025-05-02 17:14:54 -0300

    *feat* Remove preview de ListaPlanilhasScreen.kt

--- app/src/main/java/br/com/myfitt/ui/screens/ListaPlanilhasScreen.kt
+++ app/src/main/java/br/com/myfitt/ui/screens/ListaPlanilhasScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.myfitt.domain.models.Planilha
 import br.com.myfitt.ui.viewModels.ListaPlanilhasViewModel
@@ -91,8 +90,3 @@ fun ListaPlanilhasScreen(
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun PlanilhaScreenPreview() {
-//    PlanilhaScreen()
-}
