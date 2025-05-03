93e5a80313098ec90f7092249f16aad0c22ddc66
Neiscafe <guilhermeneis132@gmail.com>
2025-05-02 22:55:09 -0300

    *feat* migration recursiva

--- app/src/main/java/br/com/myfitt/data/database/TreinoDatabaseProvider.kt
+++ app/src/main/java/br/com/myfitt/data/database/TreinoDatabaseProvider.kt
@@ -2,21 +2,17 @@ package br.com.myfitt.data.database
 
 import android.content.Context
 import androidx.room.Room
+import androidx.room.RoomDatabase
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
 
 object TreinoDatabaseProvider {
     private val MIGRATION_7_8 = object : Migration(7, 8) {
         override fun migrate(db: SupportSQLiteDatabase) {
-            db.beginTransaction()
+//            db.beginTransaction()
             db.execSQL("DROP TABLE `divisao`;")
             db.execSQL("DROP TABLE `ficha`;")
             db.execSQL("DROP TABLE `ficha_exercicio`;")
-//            db.execSQL("DROP INDEX `index_ficha_exercicio_fichaId`;")
-//            db.execSQL("DROP INDEX `index_ficha_exercicio_exercicioId`;")
-//            db.execSQL("DROP INDEX `index_ficha_id`;")
-//            db.execSQL("DROP INDEX `index_ficha_divisaoId`;")
-//            db.execSQL("DROP INDEX `index_divisao_id`;")
 
             db.execSQL("CREATE TABLE IF NOT EXISTS `divisao` (`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `nome` TEXT NOT NULL);")
             db.execSQL("CREATE INDEX IF NOT EXISTS `index_divisao_id` ON `divisao` (`id`);")
@@ -28,17 +24,27 @@ object TreinoDatabaseProvider {
             db.execSQL("CREATE TABLE IF NOT EXISTS `ficha_exercicio` (`fichaId` INTEGER NOT NULL, `exercicioId` INTEGER NOT NULL, `position` INTEGER NOT NULL DEFAULT 0, PRIMARY KEY(`fichaId`, `exercicioId`), FOREIGN KEY (`fichaId`) REFERENCES `ficha` (`id`), FOREIGN KEY(`exercicioId`) REFERENCES `exercicios`(`id`));")
             db.execSQL("CREATE INDEX IF NOT EXISTS `index_ficha_exercicio_fichaId` ON `ficha_exercicio` (`fichaId`);")
             db.execSQL("CREATE INDEX IF NOT EXISTS `index_ficha_exercicio_exercicioId` ON `ficha_exercicio` (`exercicioId`);")
-            db.endTransaction()
+//            db.endTransaction()
         }
 
     }
 
     fun getDatabase(context: Context): TreinoDatabase {
-        return synchronized(this) {
-            val instance = Room.databaseBuilder(
-                context.applicationContext, TreinoDatabase::class.java, "treino_database"
-            ).addMigrations(MIGRATION_7_8).build()
-            instance
-        }
+        val instance = Room.databaseBuilder(
+            context.applicationContext, TreinoDatabase::class.java, "treino_database"
+        ).addMigrations(MIGRATION_7_8).addCallback(object : RoomDatabase.Callback() {
+            override fun onCreate(db: SupportSQLiteDatabase) {
+                super.onCreate(db)
+            }
+
+            override fun onDestructiveMigration(db: SupportSQLiteDatabase) {
+                super.onDestructiveMigration(db)
+            }
+
+            override fun onOpen(db: SupportSQLiteDatabase) {
+                super.onOpen(db)
+            }
+        }).build()
+        return instance
     }
 }
--- app/src/main/java/br/com/myfitt/ui/viewModels/ListaPlanilhasViewModel.kt
+++ app/src/main/java/br/com/myfitt/ui/viewModels/ListaPlanilhasViewModel.kt
@@ -4,13 +4,15 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import br.com.myfitt.data.repository.PlanilhaRepository
 import br.com.myfitt.domain.models.Planilha
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 
 class ListaPlanilhasViewModel(private val planilhaRepository: PlanilhaRepository) : ViewModel() {
 
     // Fluxo de Planilhas
-    val planilhas = planilhaRepository.getAllPlanilhas()
+    val planilhas = planilhaRepository.getAllPlanilhas().stateIn(viewModelScope, SharingStarted.Eagerly, emptyList())
 
     // Função para adicionar uma planilha
     fun insertPlanilha(planilha: Planilha) {
