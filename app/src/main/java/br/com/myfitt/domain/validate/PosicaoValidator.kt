26d77b38c3c5a28ccad0348ecaa0c9d6f475e6b3
Guilherme Neis <guilherme.neis@deltasul.com.br>
2025-05-13 17:10:12 -0300

    *Fix* Ajustado o valor de tolerância de alteração de posição de exercício

--- app/src/main/java/br/com/myfitt/domain/validate/PosicaoValidator.kt
+++ app/src/main/java/br/com/myfitt/domain/validate/PosicaoValidator.kt
@@ -3,6 +3,6 @@ package br.com.myfitt.domain.validate
 import br.com.myfitt.domain.models.Exercicio
 
 object PosicaoValidator {
-    fun podeAumentar(exercicio: Exercicio, lista: List<Any>) = exercicio.posicao < lista.size
+    fun podeAumentar(exercicio: Exercicio, lista: List<Any>) = exercicio.posicao < lista.size-1
     fun podeDiminuir(exercicio: Exercicio) = exercicio.posicao > 0
 }
