5e56d5dd6642fe75567ecd4253795b0fa2893f40
Guilherme Neis <guilherme.neis@deltasul.com.br>
2025-11-24 07:59:13 -0300

    *Feat* Implementa as operações do Repository no ExerciciosTreinoViewModel

--- app/src/main/java/br/com/myfitt/treinos/ui/screens/ExerciciosTreinoViewModel.kt
+++ app/src/main/java/br/com/myfitt/treinos/ui/screens/ExerciciosTreinoViewModel.kt
@@ -3,6 +3,7 @@ package br.com.myfitt.treinos.ui.screens
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import br.com.myfitt.common.domain.ExercicioTreino
+import br.com.myfitt.treinos.domain.repository.ExercicioTreinoRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -10,41 +11,61 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
-class ExerciciosTreinoViewModel(treinoId: Int) : ViewModel() {
+class ExerciciosTreinoViewModel(treinoId: Int, val treinoRepository: ExercicioTreinoRepository) :
+    ViewModel() {
     private val _state = MutableStateFlow(ExerciciosTreinoState())
     val state = _state.asStateFlow()
 
     init {
         viewModelScope.launch(Dispatchers.IO) {
-            delay(1500L)
-            val erro = "Bla bla bla bla"
+            _state.update { it.copy(carregando = true) }
+            val result = treinoRepository.lista(treinoId)
             _state.update {
                 it.copy(
-                    erro = erro, exercicios = listOf(
-                        ExercicioTreino(1, 1, 1),
-                        ExercicioTreino(2, 1, 1),
-                        ExercicioTreino(3, 1, 1),
-                    ), carregando = false
+                    erro = result.erroOrNull, exercicios = result.dataOrNull?:it.exercicios, carregando = false
                 )
             }
         }
     }
 
     fun clicks(i: Int, exercicioItem: ExercicioTreino) {
+        viewModelScope.launch(Dispatchers.IO) {
+            _clicks(i, exercicioItem)
+        }
+    }
+
+    suspend fun _clicks(i: Int, exercicioItem: ExercicioTreino) {
         when (i) {
             cardClick -> {
-                _state.update { it.copy(erro = "Séries detalhadas: Não implementado.") }
+
             }
+
             removerClick -> {
-//                _state.update { it.copy(exercicios = it.exercicios.minus(exercicioItem)) }
-                _state.update { it.copy(erro = "Remover: Não implementado.") }
+                _state.update { it.copy(carregando = true) }
+                val result = treinoRepository.remove(exercicioItem)
+                _state.update {
+                    it.copy(
+                        carregando = false,
+                        erro = result.erroOrNull,
+                        exercicios = result.dataOrNull ?: it.exercicios
+                    )
+                }
             }
 
             substituirClick -> {
-                _state.update { it.copy(erro = "Substituir: Não implementado.") }
+
             }
+
             arrastarClick -> {
-                _state.update { it.copy(erro = "Arrastar: Não implementado.") }
+                _state.update { it.copy(carregando = true) }
+                val result = treinoRepository.reordena(exercicioItem, exercicioItem.ordem)
+                _state.update {
+                    it.copy(
+                        carregando = false,
+                        erro = result.erroOrNull,
+                        exercicios = result.dataOrNull ?: it.exercicios
+                    )
+                }
             }
         }
     }
