258657330feb1c0ceafea7e0f9db616b7c27f9eb
Guilherme Neis <guilherme.neis@deltasul.com.br>
2025-11-24 14:01:30 -0300

    *Feat* Pesquisa de exercício em ExercicioRepository

---
+++ app/src/main/java/br/com/myfitt/treinos/data/repository/ExercicioRepositoryImpl.kt
@@ -0,0 +1,19 @@
+package br.com.myfitt.treinos.data.repository
+
+import br.com.myfitt.common.domain.Exercicio
+import br.com.myfitt.common.domain.Resultado
+import br.com.myfitt.treinos.domain.repository.ExercicioRepository
+import kotlinx.coroutines.delay
+
+class ExercicioRepositoryImpl : ExercicioRepository {
+    private val exercicios: List<Exercicio> = listOf(
+        Exercicio(1, "Supino reto", null),
+        Exercicio(2, "Passada", null),
+        Exercicio(3, "Rosquinha", null),
+    )
+
+    override suspend fun lista(pesquisa: String): Resultado<List<Exercicio>> {
+        delay(1500)
+        return Resultado.Sucesso(exercicios.filter { it.nome.contains(pesquisa) })
+    }
+}
