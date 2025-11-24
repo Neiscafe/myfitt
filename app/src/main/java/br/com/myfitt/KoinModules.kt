c50dc938ae83744192c976d6119022012c4ac7ea
Guilherme Neis <guilherme.neis@deltasul.com.br>
2025-11-24 14:07:30 -0300

    *Feat* Adiciona ListaExercicioViewModel e Repository ao KoinModules

--- app/src/main/java/br/com/myfitt/KoinModules.kt
+++ app/src/main/java/br/com/myfitt/KoinModules.kt
@@ -1,8 +1,11 @@
 package br.com.myfitt
 
+import br.com.myfitt.treinos.data.repository.ExercicioRepositoryImpl
 import br.com.myfitt.treinos.data.repository.ExercicioTreinoRepositoryImpl
+import br.com.myfitt.treinos.domain.repository.ExercicioRepository
 import br.com.myfitt.treinos.domain.repository.ExercicioTreinoRepository
 import br.com.myfitt.treinos.ui.screens.exerciciosTreino.ExerciciosTreinoViewModel
+import br.com.myfitt.treinos.ui.screens.listaExercicios.ListaExerciciosViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
@@ -14,8 +17,10 @@ val daoModule = module {
 }
 val repositoryModule = module {
     single<ExercicioTreinoRepository> { ExercicioTreinoRepositoryImpl() }
+    single<ExercicioRepository> { ExercicioRepositoryImpl() }
 }
 val viewModelModule = module {
     viewModel { ExerciciosTreinoViewModel(it[0], get()) }
+    viewModel { ListaExerciciosViewModel(get()) }
 }
 val appModule = listOf(repositoryModule, daoModule, databaseModule, viewModelModule)
