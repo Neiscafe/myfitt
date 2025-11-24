7096282e63d336b33089f14beff59e30ee3a6c06
Guilherme Neis <guilherme.neis@deltasul.com.br>
2025-11-24 11:51:11 -0300

    *Feat* ExerciciosViewModel para tela de pesquisa de exercícios

---
+++ app/src/main/java/br/com/myfitt/treinos/ui/screens/exercicios/ExerciciosViewModel.kt
@@ -0,0 +1,38 @@
+package br.com.myfitt.treinos.ui.screens.exercicios
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import br.com.myfitt.common.domain.Exercicio
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class ExerciciosViewModel : ViewModel() {
+    private var pesquisa: String = ""
+    private var pesquisaJob: Job? = null
+
+    companion object {
+        private var callback: ((Exercicio) -> Unit)? = null
+        fun setCallback(callback: (Exercicio) -> Unit) {
+            this.callback = callback
+        }
+    }
+
+    init {
+        callback = null
+    }
+
+    override fun onCleared() {
+        callback = null
+    }
+
+    fun pesquisaMudou(pesquisa: String) {
+        this.pesquisa = pesquisa.trim()
+        if (this.pesquisa.length < 3) return
+        pesquisaJob?.cancel()
+        pesquisaJob = viewModelScope.launch(Dispatchers.IO) {
+            delay(500L)
+        }
+    }
+}
