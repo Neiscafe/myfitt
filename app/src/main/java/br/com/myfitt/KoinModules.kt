da5a0f1bd06538231ec29dda4ea1965141c37f53
Guilherme Neis <guilherme.neis@deltasul.com.br>
2025-11-24 07:59:28 -0300

    *Feat* ExercicioTreinoRepository no koinModules

--- app/src/main/java/br/com/myfitt/KoinModules.kt
+++ app/src/main/java/br/com/myfitt/KoinModules.kt
@@ -1,5 +1,7 @@
 package br.com.myfitt
 
+import br.com.myfitt.treinos.data.repository.ExercicioTreinoRepositoryImpl
+import br.com.myfitt.treinos.domain.repository.ExercicioTreinoRepository
 import br.com.myfitt.treinos.ui.screens.ExerciciosTreinoViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
@@ -11,9 +13,9 @@ val daoModule = module {
 
 }
 val repositoryModule = module {
-
+    single<ExercicioTreinoRepository> { ExercicioTreinoRepositoryImpl() }
 }
 val viewModelModule = module {
-    viewModel { ExerciciosTreinoViewModel(it[0]) }
+    viewModel { ExerciciosTreinoViewModel(it[0], get()) }
 }
 val appModule = listOf(repositoryModule, daoModule, databaseModule, viewModelModule)
