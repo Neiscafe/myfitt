cfc22eca410bd601e5b7b3c2a867ce181ad4dd64
Guilherme Neis <guilherme.neis@deltasul.com.br>
2025-11-27 08:06:27 -0300

    *Feat* Adiciona SeriesRepository ao KoinModules

--- app/src/main/java/br/com/myfitt/KoinModules.kt
+++ app/src/main/java/br/com/myfitt/KoinModules.kt
@@ -2,9 +2,11 @@ package br.com.myfitt
 
 import br.com.myfitt.treinos.data.repository.ExercicioRepositoryImpl
 import br.com.myfitt.treinos.data.repository.ExercicioTreinoRepositoryImpl
+import br.com.myfitt.treinos.data.repository.SeriesRepositoryImpl
 import br.com.myfitt.treinos.data.repository.TreinoRepositoryImpl
 import br.com.myfitt.treinos.domain.repository.ExercicioRepository
 import br.com.myfitt.treinos.domain.repository.ExercicioTreinoRepository
+import br.com.myfitt.treinos.domain.repository.SeriesRepository
 import br.com.myfitt.treinos.domain.repository.TreinoRepository
 import br.com.myfitt.treinos.ui.screens.exerciciosTreino.ExerciciosTreinoViewModel
 import br.com.myfitt.treinos.ui.screens.listaExercicios.ListaExerciciosViewModel
@@ -22,6 +24,7 @@ val repositoryModule = module {
     single<ExercicioTreinoRepository> { ExercicioTreinoRepositoryImpl() }
     single<ExercicioRepository> { ExercicioRepositoryImpl() }
     single<TreinoRepository> { TreinoRepositoryImpl() }
+    single<SeriesRepository> { SeriesRepositoryImpl() }
 }
 val viewModelModule = module {
     viewModel { ExerciciosTreinoViewModel(it[0], get()) }
