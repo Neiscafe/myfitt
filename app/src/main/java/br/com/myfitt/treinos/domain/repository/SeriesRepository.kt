39bc54c08b74af3c50bcb6bda0f2e20030e4e356
Guilherme Neis <guilherme.neis@deltasul.com.br>
2025-11-27 07:59:29 -0300

    *Fix* Adiciona exercicioTreinoId ao método de lista de SeriesRepository

--- app/src/main/java/br/com/myfitt/treinos/domain/repository/SeriesRepository.kt
+++ app/src/main/java/br/com/myfitt/treinos/domain/repository/SeriesRepository.kt
@@ -4,7 +4,7 @@ import br.com.myfitt.common.domain.Resultado
 import br.com.myfitt.common.domain.SerieExercicio
 
 interface SeriesRepository {
-    suspend fun lista(): Resultado<List<SerieExercicio>>
+    suspend fun lista(exercicioTreinoId: Int): Resultado<List<SerieExercicio>>
     suspend fun cria(serie: SerieExercicio): Resultado<List<SerieExercicio>>
     suspend fun altera(alterada: SerieExercicio): Resultado<List<SerieExercicio>>
 }
