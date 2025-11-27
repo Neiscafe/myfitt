caf058b9d518b44e5fb87c914e94e6697bdcde16
Guilherme Neis <guilherme.neis@deltasul.com.br>
2025-11-27 07:58:38 -0300

    *Feat* Melhoria na tipagem de Resultado.Erro se tornando Nothing

--- app/src/main/java/br/com/myfitt/common/domain/Resultado.kt
+++ app/src/main/java/br/com/myfitt/common/domain/Resultado.kt
@@ -4,11 +4,11 @@ package br.com.myfitt.common.domain
 
 sealed class Resultado<out T> {
     data class Sucesso<T>(val data: T) : Resultado<T>()
-    data class Erro<T>(val erro: String?) : Resultado<T>()
+    data class Erro(val erro: String?) : Resultado<Nothing>()
 
     val sucesso = this is Sucesso
-    val erroOrNull get()= (this as? Erro)?.erro
-    val dataOrNull get()= (this as? Sucesso)?.data
+    val erroOrNull get() = (this as? Erro)?.erro
+    val dataOrNull get() = (this as? Sucesso)?.data
 }
 
 fun <T, R> Resultado<T>.map(block: (T) -> R = { it as R }): Resultado<R> {
